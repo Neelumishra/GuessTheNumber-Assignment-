456e92f43d16a8897bd6b93bdc4ffe2915c8d964
Neelumishra <119746262+Neelumishra@users.noreply.github.com>
2023-03-30 13:11:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GuessTheNumber-Assignment-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-seksmd)
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+function GuessingGame() {
+  const [luckyNumber, setLuckyNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  const [guess, setGuess] = useState('');
+  const [message, setMessage] = useState('');
+  const [attempts, setAttempts] = useState(0);
+
+  function handleGuess(event) {
+    event.preventDefault();
+    const numGuess = parseInt(guess);
+    if (isNaN(numGuess)) {
+      setMessage('Please enter a valid number.');
+    } else if (numGuess < luckyNumber) {
+      setAttempts(attempts + 1);
+      setMessage(`You guessed ${numGuess}. That's too low!`);
+    } else if (numGuess > luckyNumber) {
+      setAttempts(attempts + 1);
+      setMessage(`You guessed ${numGuess}. That's too high!`);
+    } else {
+      setAttempts(attempts + 1);
+      setMessage(`Congratulations! You guessed the number ${luckyNumber} in ${attempts + 1} attempts!`);
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleGuess}>
+        <label>
+          Guess the number (1-100):
+          <input type="text" value={guess} onChange={event => setGuess(event.target.value)} />
+        </label>
+        <button type="submit">Guess</button>
+      </form>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default GuessingGame;
+
